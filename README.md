003870a991552f3fd04bf74322bc19e468fcd631
JHorst2020 <69328659+JHorst2020@users.noreply.github.com>
2021-09-28 22:26:53 -0700

    reuploading package

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# format-phone-number
+# format-telephone
+
+Format-telephone provides a quick way to convert a string into a formatted telephone number. It also provides a way to convert an alphanumeric string into a normal phone number (eg 1-800-CALL-ATT into 1-800-225-5288). 
+
+## Installation
+
+```bash
+npm install format-telephone
+```
+
+## Usage
+
+```javascript
+const {formatPhoneNum, allDigitsEntered} = require ('format-telephone')
+
+// Convert String or Number
+formatPhoneNum("5552220000") // Returns (555) 222-0000
+
+// Convert String or Number with Template
+formatPhoneNum("5552220000", "###-###-####") // Returns 555-222-0000
+
+// Convert Keypad Entry with Template 
+formatPhoneNum("800CALLATT", "###.###.####", true) // Returns 800.225.5288
+
+// Verify that Enough Digits have been Entered
+allDigitsEntered("(555) 222-#### ") // Returns false
+allDigitsEntered("(555) 222-0000 ") // Returns true
+```
+## Variables
+
+```javascript
+formatPhoneNum(input, template, keypad)
+// input: String or Number input.
+// template: String containing octothorps (#).      Default --> "(###) ###-####"
+// keypad: Boolean.                                 Default --> false
+// returns string
+
+allDigitsEntered(input)
+// input: String or Number input
+// returns boolean --> false indicates not enough digits have been entered
+
+```
+## Edge Cases
+
+```javascript
+// Too many digits
+formatPhoneNum(999111111111111111,"###-####") // Returns 999-1111
+
+// Not enough digits
+formatPhoneNum(1234) // Returns (123) 4##-####
+
+// No octothorps in template
+formatPhoneNum("555 2222", "xxx-xxxx") // Returns xxx-xxxx
+
+// Not Indicating a Keypad Entry
+formatPhoneNum("800CALLATT", null, false) // Returns (800) ###-####
+```
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,80 @@
+const isNumRecursion = (input, keypad, result = '') => {
+    let char = input.slice(0,1)
+    let numbers = ''
+    if(!isNaN(parseInt(char))){
+        numbers = char.toString()
+    }
+    if(keypad === true && isNaN(parseInt(char))){
+        let lowerCaseChar = char.toLowerCase()
+        if(["a", "b", "c"].indexOf(lowerCaseChar) !== -1){
+            numbers = 2
+        }
+        if(["d", "e", "f"].indexOf(lowerCaseChar) !== -1){
+            numbers = 3
+        }
+        if(["g", "h", "i"].indexOf(lowerCaseChar)!==-1){
+            numbers = 4
+        }
+        if(["j", "k", "l"].indexOf(lowerCaseChar) !== -1){
+            numbers = 5
+        }
+        if(["m", "n", "o"].indexOf(lowerCaseChar) !== -1){
+            numbers = 6
+        }
+        if(["p", "q", "r","s"].indexOf(lowerCaseChar) !== -1){
+            numbers = 7
+        }
+        if(["t", "u", "v"].indexOf(lowerCaseChar) !== -1){
+            numbers = 8
+        }
+        if(["w", "x", "y","z"].indexOf(lowerCaseChar) !== -1){
+            numbers = 9
+        }
+    }
+    if(input.length > 0){
+       return isNumRecursion(input.slice(1), keypad, `${result}${numbers}` )
+    } else {
+        return result.split('')
+    }
+}
+
+const phoneNumResult = (numArray, value = []) => {
+    const currNum = numArray.shift()
+    const octothorpIndex = value.indexOf('#')
+    if(octothorpIndex !== -1){
+        value.splice(octothorpIndex,1, currNum)
+    }
+    if(numArray.length === 0 || octothorpIndex === -1){
+        return value.join('')
+    }
+    return phoneNumResult(numArray, value)
+}
+
+
+const formatPhoneNum = (phoneNumString, formatTemplate = "(###) ###-####", keypadBool = false) => {
+    let phoneNum = phoneNumString
+    let template = formatTemplate == null ? ("(###) ###-####").split("") : formatTemplate.split("")
+    let keypad = keypadBool
+    if(typeof keypad !== "boolean"){
+        keypad = false
+    }
+    if(typeof phoneNum !== "string" && typeof phoneNum !== "number" || phoneNum == null || phoneNum==false){
+        return "Error: Input must be a string or number"
+    }
+    if(typeof phoneNum === "number"){
+        phoneNum = phoneNum.toString()
+    }
+    const parsedPhoneNum = isNumRecursion(phoneNum, keypad)
+    const formatTele = phoneNumResult(parsedPhoneNum, template)
+    return(formatTele)
+}
+
+const allDigitsEntered = (input) => {
+    if(typeof input !== "string" && typeof input !== "number" || input == null || input == false ){
+        return false
+    }
+    console.log(input)
+    return input.toString().split("").indexOf('#') === -1
+}
+
+module.exports = {formatPhoneNum, allDigitsEntered}
